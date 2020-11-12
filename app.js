a3ede211e8406b1e3d27bec51d27b4eb77974b6f
Zac Olds <zac.olds@gmail.com>
2020-11-12 10:15:27 -0600

    appending data to the DOM

--- app.js
+++ app.js
@@ -10,32 +10,50 @@
 // `https://api.openbrewerydb.org/breweries?by_postal=${zip}`
 
 
-// brewData will fetch data from the openbrewerydb api and return it as an array of objects.
+
+
+// ======================================================
+// brewData - will fetch data from the openbrewerydb api and return it as an array of objects.
+// ======================================================
 const brewData = async (brewery) => {
   try {
     const url = await axios.get(`https://api.openbrewerydb.org/breweries/search?query=${brewery}`);
-    console.log(url.data)
+    console.log(url.data[0].name);
+    showBrewInfo(url.data[0]);
     return url;
   } catch (error) {
     console.alert(`Something is Wrong: ${error}`)
   }
-}
+};
 
-brewData(`Goose`);
-
-// event listener
+// ======================================================
+// brewSearch - adds an event listener to the brewery search input and calls brewData with the value that is returned. Text input can be the name of a brewery, a zip code, or a city.
+// ======================================================
 const brewSearch = () => {
   let searchInput = document.querySelector(`#search-button`);
   searchInput.addEventListener('click', (event) => {
     event.preventDefault
     const searchText = document.querySelector('#brew-search-input');
-    console.log(searchText.value);
+    brewData(searchText.value);
   })
 }
 
 brewSearch();
 
-// add search results
+// ======================================================
+// showBrewInfo - will take data obtained from brewData and append it to the brewery info section of the website.
+// ======================================================
+const showBrewInfo = (brewData) => {
+  let brewInfo = 
+    `
+    <h3>${brewData.name}</h3>
+    <h4>${brewData.city}, ${brewData.state}</h4>
+    `;
+  let container = document.querySelector('#brewery-info');
+  container.insertAdjacentHTML('beforeend', brewInfo);
+}
 
-
+// ======================================================
 // function to remove previous search results
+// ======================================================
+
